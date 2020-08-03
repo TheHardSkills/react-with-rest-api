29f4b843dcbb8a453abfc5750ce6c030947cdb26
YuliaOkushko <juli.okushko@gmail.com>
2020-08-03 10:49:44 +0300

    Final edits

--- src/components/Month.jsx
+++ src/components/Month.jsx
@@ -15,7 +15,7 @@ const Month = (props) => {
   return (
     <div className="card month" id={monthId}>
       <img className="card-img-top" src={month.src} alt={month.title} />
-      <div class="descr">{listItems !== 0 && <ul>{listItems}</ul>}</div>
+      <div class="birthdayList">{listItems !== 0 && <ul>{listItems}</ul>}</div>
     </div>
   );
 };
