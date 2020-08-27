86d07543ed1b8b78972f529e876c85cfba6f9a8f
YuliaOkushko <juli.okushko@gmail.com>
2020-08-27 11:55:10 +0300

    Correction of the logic of component 'Month'

--- src/components/Month.jsx
+++ src/components/Month.jsx
@@ -8,22 +8,22 @@ class Month extends React.Component {
 
   render() {
     const { month, usersData } = this.props;
-
     this.monthId = month.title;
-    let listItems = 0;
-    if (usersData !== null) {
-      listItems = usersData.map((oneUserDate) => {
-        return <li>{`${oneUserDate.firstName} ${oneUserDate.lastName}`}</li>;
-      });
-    } else {
-      listItems = <li>Wait..</li>;
-    }
 
     return (
       <div className="card month" id={this.monthId}>
         <img className="card-img-top" src={month.src} alt={month.title} />
         <div class="birthdayList">
-          {listItems !== 0 && <ul>{listItems}</ul>}
+          {usersData !== null && (
+            <ul>
+              {usersData.map((oneUserDate) => {
+                return (
+                  <li>{`${oneUserDate.firstName} ${oneUserDate.lastName}`}</li>
+                );
+              })}
+            </ul>
+          )}
+          {usersData === null && <p>Wait..</p>}
         </div>
       </div>
     );
